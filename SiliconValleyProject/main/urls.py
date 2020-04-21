8a08315f95c3be690c2ec115936e86610c548ff7
SohamGupta21 <62520353+SohamGupta21@users.noreply.github.com>
2020-04-21 17:52:26 -0500

    added blog application

--- SiliconValleyProject/main/urls.py
+++ SiliconValleyProject/main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('shelter', views.shelter, name='shelter'),
-    path('restaurant', views.restaurant, name='restaurant')
+    path('shelter/', views.shelter, name='shelter'),
+    path('restaurant/', views.restaurant, name='restaurant')
 ]
