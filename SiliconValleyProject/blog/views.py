3ffa763568c5d9c11c7c709a272b31e13a21a6d3
SohamGupta21 <62520353+SohamGupta21@users.noreply.github.com>
2020-04-22 13:19:38 -0500

    with title jinja changes

--- SiliconValleyProject/blog/views.py
+++ SiliconValleyProject/blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def blog(request):
-    return render(request, 'blog-home.html')
+    return render(request, 'blog-home.html', {'title':"FWISB Blog Home"})
