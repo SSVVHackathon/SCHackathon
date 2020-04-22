98d1a4a6d330a7acdaebde565a694d470e345a72
SohamGupta21 <62520353+SohamGupta21@users.noreply.github.com>
2020-04-22 16:55:13 -0500

    this

--- SiliconValleyProject/main/views.py
+++ SiliconValleyProject/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'title':'FWISB Home'})
+    return render(request, 'home.html',{'title':'FWISB Home','homepage':'true','heading':'Food Where It Should Be'})
 
 def shelter(request):
-    return render(request, 'shelter.html', {'title':"FWISB Shelter"})
+    return render(request, 'shelter.html', {'title':"FWISB Shelter",'heading':'Shelter'})
 
 def restaurant(request):
-    return render(request, 'restaurant.html',{'title':"FWISB Restaurant"})
+    return render(request, 'restaurant.html',{'title':"FWISB Restaurant",'heading':'Restaurant'})
