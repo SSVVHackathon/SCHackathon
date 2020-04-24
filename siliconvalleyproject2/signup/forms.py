5284b0e7244187739fc092949d758624935d27e8
Socksham <60550018+Socksham@users.noreply.github.com>
2020-04-24 14:25:42 -0500

    signup in custom model

--- siliconvalleyproject2/signup/forms.py
+++ siliconvalleyproject2/signup/forms.py
@@ -4,4 +4,4 @@ from . models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['company_name', 'email', 'username', 'password', 'address']
+        fields = ['company_name', 'email', 'password', 'address']
--- siliconvalleyproject2/signup/views.py
+++ siliconvalleyproject2/signup/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect, HttpResponse
+from . forms import ProfileForm
 # from .forms import ProfileForm
 # Create your views here.
 
 def signup(request):
-    return HttpResponse("HI")
+    form = ProfileForm(request.POST)
+    if form.is_valid():
+        form.save()
+        form = ProfileForm()
+
+    return render(request, "signup.html", {'form':form})
