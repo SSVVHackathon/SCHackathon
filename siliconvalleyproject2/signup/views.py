27c9a222a4043a3f89044ff577fded454b91019d
SohamGupta21 <62520353+SohamGupta21@users.noreply.github.com>
2020-04-25 10:45:14 -0500

    working on json

--- siliconvalleyproject2/signup/views.py
+++ siliconvalleyproject2/signup/views.py
@@ -1,12 +1,23 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect, HttpResponse
 from . forms import ProfileForm
+import json
 # from .forms import ProfileForm
 # Create your views here.
 
 def signup(request):
     form = ProfileForm(request.POST)
     if form.is_valid():
+        print(form.cleaned_data.get('address'))
+        # data = {
+        #     "address":f"{form.cleaned_data.get('address')}",
+        #     "company_name":f'{form.cleaned_data.get("company_name")}',
+        #     "email":f'{form.cleaned_data.get("email")}'
+        # }
+        # with open('sign_up_data.json','w') as j:
+        #     file_data = json.load(j)
+        #     file_data['companies'].append(data)
+        #     json.dump(file_data,j)
         form.save()
         form = ProfileForm()
     return render(request, "signup.html", {'form':form})
