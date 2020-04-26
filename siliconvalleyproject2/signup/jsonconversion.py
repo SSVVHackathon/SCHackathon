3cf3d4cc8411b86a1352c206178c53c7e1fa5e0c
Socksham <60550018+Socksham@users.noreply.github.com>
2020-04-26 04:56:20 -0500

    iwgierfg

--- siliconvalleyproject2/signup/jsonconversion.py
+++ siliconvalleyproject2/signup/jsonconversion.py
@@ -3,12 +3,12 @@ def conversion(lat, lng, email, company_name, shelter_or_restaurant, address):
     with open('templates/blah.json') as f:
         data = json.load(f)
     new_info = {
-        'address':address,
-        'lat':lat,
-        'lng':lng,
-        'shelter_or_restaurant':shelter_or_restaurant,
-        'email':email,
-        'company_name':company_name
+        "address":address,
+        "lat":lat,
+        "lng":lng,
+        "shelter_or_restaurant":shelter_or_restaurant,
+        "email":email,
+        "company_name":company_name
     }
     data['companies'].append(new_info)
     with open('templates/blah.json', 'w') as f:
